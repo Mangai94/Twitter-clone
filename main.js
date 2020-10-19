2ef16bf77e7f119eb629cefe87accba66673f2fb
Mangayarkarasi Jeyachandran <mangayarkarasi.j@GAVSTECH.COM>
2020-10-19 22:28:06 +0530

    added other headers

--- main.js
+++ main.js
@@ -10,7 +10,6 @@ var accessTokenSecret = 'BNq9pwLXLdDSUAO1w2j2hAbYbRs7NDjt9AFzoZCuIoLPF';
 var nonce = 'K7ny27JTpKVsTgdyLdDfmQQWVLERj2zAK5BslRsqyw';
 var signature = 'Pc%2BMLdv028fxCErFyi8KXFM%2BddU%3D';
 var callbackUrl = "https%3A%2F%2Fnostalgic-lalande-f9f860.netlify.app%2FHome.html";
-//var callbackUrl = "C%3AUsersmangayarkarasi.jDocumentsGuviTwitter%20cloneHome.html";
 var timestamp = '1364161424';
 
 function loginRequest()
@@ -42,18 +41,13 @@ function loginRequest()
     });
 }
 
-//var auth = 'OAuth oauth_consumer_key="OsP4fYleWKJceK5CyPkQdscYa",oauth_token="449848553-G9OPRRYStA9WmDvS9iZcX3wBRNgvWQtplLHprmkD",oauth_signature_method="HMAC-SHA1",oauth_timestamp="1603028813",oauth_nonce="IdGko9EXuZm",oauth_version="1.0",oauth_callback="https%3A%2F%2Fnostalgic-lalande-f9f860.netlify.app%2FHome.html",oauth_signature="0hD8frUYHuzDjcIXUBv9VgUf8MA%3D"'
-
 function getAuthorization()
 {
     return 'OAuth '                                         +
-        'oauth_callback="' + encodeURIComponent('https://nostalgic-lalande-f9f860.netlify.app/Home.html')      + '", '+
+        'oauth_callback="' + callbackUrl     + '", '+
         'oauth_consumer_key="'  + apiKey       + '", ' +
-        'oauth_consumer_secret="'  + apiSecretKey       + '", ' +
         'oauth_nonce="'         + nonce             + '", ' +
         'oauth_signature="'     + signature         + '", ' +
         'oauth_signature_method="HMAC-SHA1", '              +
-        // 'oauth_timestamp="'     + timestamp         + '", ' +
-        // 'oauth_token="'         + accessToken       + '", ' +
         'oauth_version="1.0"'     
 }
